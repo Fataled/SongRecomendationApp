773ede5fd4f7f64930e5d063f02327ad157dd4d8
Fataled <Fataledxd@gmail.com>
2026-03-03 23:56:47 -0500

    Inital Commnit

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ProjectHellsParadise;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
