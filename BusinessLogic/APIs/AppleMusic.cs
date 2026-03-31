81f40648a43281a1d9618c205c8e36215f94b42e
Brume <brumeako@gmail.com>
2026-03-31 11:54:06 -0400

    rithvik do

--- BusinessLogic/APIs/AppleMusic.cs
+++ BusinessLogic/APIs/AppleMusic.cs
@@ -1,5 +1,8 @@
 ﻿namespace ProjectHellsParadise.BusinessLogic.APIs;
 
+/// <summary>
+/// RITHVIK DO THIS AND DO THE APPLE MUSIC CONNECTION PAGE ALSO MAKE THE SETTINGS XAML HAVE TABS: ONE FOR SPOTIFY ONE FOR APPLE ONE FOR USER
+/// </summary>
 public class AppleMusic : ApiClientBase
 {
     public AppleMusic() : base("")
