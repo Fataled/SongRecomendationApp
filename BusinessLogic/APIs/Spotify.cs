fe05ddaca08c51743ef5f980569015266faf8133
Obaid Waqas <obaidwaqas73@gmail.com>
2026-04-10 19:36:55 -0400

    Needs token

--- BusinessLogic/APIs/Spotify.cs
+++ BusinessLogic/APIs/Spotify.cs
@@ -1,7 +1,14 @@
 ﻿namespace ProjectHellsParadise.BusinessLogic.APIs;
 
+
+/// <summary>
+/// Talks to the Spotify Web API
+/// Inherits from the ApiClientBase class
+/// </summary>
 public class SpotifyClient : ApiClientBase
 {
+    private string _token = "";
+
     public SpotifyClient() : base("https://api.spotify.com/v1")
     {
         
